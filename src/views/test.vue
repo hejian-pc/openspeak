2b5fb62994e7cf85d541f6165cc20083999effee
HeJian <hejmls@163.com>
2024-05-12 13:58:12 +0800

    测试更新

--- src/views/test.vue
+++ src/views/test.vue
@@ -1,14 +1,5 @@
 <template>
-    <div>
-    <h2>帖子列表</h2>
-    <ul>
-      <li v-for="post in posts" :key="post.id">
-        <h3>{{ post.title }}</h3>
-        <p>{{ post.content }}</p>
-        <p>作者: {{ post.author }}</p>
-      </li>
-    </ul>
-  </div>
+    测试功能
 </template>
 <script>
 import { defineComponent } from '@vue/composition-api'
